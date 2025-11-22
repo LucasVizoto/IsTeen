0134d8043ea6ce4ea368518aecb18e08cb4bc523
Lucas Vizoto <lucasvizoto364@gmail.com>
2025-11-22 10:42:49 -0300

    feat: implementing create game with tests

---
+++ backend/src/use-cases/create-game-use-case.ts
@@ -0,0 +1,39 @@
+import type { GamesRepository } from "@/repositories/games-repository.js"
+import type { Game } from "generated/prisma/index.js"
+
+interface CreateGameUseCaseRequest{
+    game_name: string
+    game_description:string
+    release_date: Date
+    url_game: string
+    url_image_game: string
+    developer: string
+}
+
+interface CreateGameUseCaseResponse{
+    game: Game
+}
+
+
+export class CreateGameUseCase{
+    constructor(private gamesRepository: GamesRepository){}
+
+    async execute({game_name, game_description, release_date,url_game,url_image_game, developer}:CreateGameUseCaseRequest): Promise<CreateGameUseCaseResponse> {
+
+
+        const game = await this.gamesRepository.create({
+            game_name,
+            game_description,
+            release_date,
+            url_game,
+            url_image_game,
+            developer
+        })
+
+        return {
+            game,
+        }
+    }
+}
+
+
