8c0e8d1617823ad8dec7c1de01dc2c0846941f8b
Lucas Vizoto <lucasvizoto364@gmail.com>
2025-12-01 21:37:39 -0300

    feat: adding register user on project with tests an in memory/repository

---
+++ backend/src/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,46 @@
+import { type User, type Prisma, Role } from "generated/prisma/index.js";
+import type { UserRepository } from "../users-repository.js";
+import { randomUUID } from "node:crypto";
+
+export class InMemoryUsersRepository implements UserRepository{
+    
+    public items: User[] = []
+    
+    async findById(userId: string): Promise<User | null> {
+        const user = this.items.find((item) => item.id == userId)
+
+        if (!user){
+            return null
+        }
+
+        return user
+    }
+
+
+    async findByEmail(email: string){
+        const user = this.items.find(item => item.email == email)
+
+        if (!user){
+            return null
+        }
+
+        return user
+    }
+
+    async create(data: Prisma.UserCreateInput) {
+        const user = {
+            id: randomUUID(),
+            username: data.username,
+            email: data.email,
+            password: data.password,
+            role: Role.USER,
+            created_at: new Date(),
+            updated_at: new Date(),
+        }
+
+        this.items.push(user)
+
+        return user
+    }
+
+}
---
+++ backend/src/use-cases/register-user-use-case.spec.ts
@@ -0,0 +1,71 @@
+import {expect, it, describe, beforeEach} from 'vitest'
+import {InMemoryUsersRepository} from 'src/repositories/in-memory/in-memory-users-repository.js'
+import { compare } from 'bcryptjs'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error.js'
+import { RegisterUseCase } from './register-user-use-case.js'
+
+let userRepository: InMemoryUsersRepository
+let sut: RegisterUseCase
+
+describe('Register Use-Case', () =>{
+    
+    beforeEach(()=>{
+        userRepository = new InMemoryUsersRepository()
+        sut = new RegisterUseCase(userRepository)
+    })
+
+    it('should be able to register', async () =>{
+
+        const {user} = await sut.execute({
+            name: 'Jhon Doe',
+            email: 'johndoe@example.com',
+            password: '123456'
+        })
+
+        expect(user.id).toEqual(expect.any(String))
+        //aqui eu digo basicamente que eu espero que o user id seja igual a qualquer string
+    })
+
+
+
+    it('should hash user password upon registration', async () =>{
+
+        const {user} = await sut.execute({
+            name: 'Jhon Doe',
+            email: 'johndoe@example.com',
+            password: '123456'
+        })
+        
+        const isPassowrdCorrectlyHashed = await compare(
+            '123456',
+            user.password
+        )
+        //aqui eu faço a comparação se o valor que eu se assemelha ao hash
+        
+        expect(isPassowrdCorrectlyHashed).toBe(true)
+    })
+
+
+    it('should not be able to register with same email twice', async () =>{
+
+        const email = 'johndoe@example.com'
+
+        await sut.execute({
+            name: 'Jhon Doe',
+            email,
+            password: '123456'
+        })
+
+        await expect(() =>
+            sut.execute({
+                name: 'Jhon Doe',
+                email,
+                password: '123456'
+            }),
+        ).rejects.toBeInstanceOf(UserAlreadyExistsError)
+        //neste rejects é basicamente o raises do python
+        //eu espero que dÊ algum erro nessa execução e posso dizer até mesmo
+        // qual dos meus erro que ele é, usando o isInstanceOf
+    })
+
+})
