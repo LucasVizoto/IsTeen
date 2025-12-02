88b2ee7387460f73e69b70e576439c86fe7e3943
Lucas Vizoto <lucasvizoto364@gmail.com>
2025-12-01 22:03:16 -0300

    feat: implementing get user profile informations with tests

---
+++ backend/src/use-cases/get-user-profile.ts
@@ -0,0 +1,31 @@
+import type { UserRepository } from "@/repositories/users-repository.js";
+import type { User } from "generated/prisma/index.js";
+import { NotFoundError } from "./errors/not-found-error.js";
+
+interface GetProfileUseCaseRequest{
+    userId: string
+}
+
+interface GetProfileUseCaseResponse {
+    user: User
+}
+
+export class GetProfileUseCase{
+    constructor(
+        private userRepository: UserRepository,
+    ) {}
+
+    async execute({userId}: GetProfileUseCaseRequest): Promise<GetProfileUseCaseResponse>{
+        //buscar o usuário no banco pelo e-mail
+        //compaarar se a senha salva no banco bate com a senha do param
+        const user = await this.userRepository.findById(userId)
+
+        if (!user){
+            throw new NotFoundError()
+        }
+
+        return{
+            user,
+        }
+    }
+}
