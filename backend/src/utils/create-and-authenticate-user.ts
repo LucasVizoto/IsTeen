cb130b34d78df24178e525bc54f602216e7ca8f1
Lucas Vizoto <lucasvizoto364@gmail.com>
2025-12-02 22:39:08 -0300

    feat: implementing register user on application with tests and utils

---
+++ backend/src/utils/create-and-authenticate-user.ts
@@ -0,0 +1,27 @@
+import { prisma } from '@/lib/prisma.js'
+import { hash } from 'bcryptjs'
+import type { FastifyInstance } from 'fastify'
+import request from 'supertest'
+
+export async function createAndAuthenticateUser(app: FastifyInstance, isAdmin = false){
+    const user = await prisma.user.create({
+        data:{
+            username:'John Doe',
+            email: 'johndoe@example.com',
+            password: await hash('123456', 6),
+            role: isAdmin ? 'ADMIN' : 'USER',
+        }
+    })
+
+    const authResponse = await request(app.server)
+    .post('/sessions')
+    .send({
+        email: 'johndoe@example.com',
+        password:'123456',
+    })
+
+    const {token} = authResponse.body
+    return {
+        token,
+    }
+}
